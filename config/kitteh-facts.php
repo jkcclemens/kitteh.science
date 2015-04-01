bf4ed56962f1893343d7e3afc67e5e35928c52e1
Kyle Clemens <jkc.clemens@gmail.com>
2015-04-01 16:15:44 -0400

    Added: More facts

--- config/kitteh-facts.php
+++ config/kitteh-facts.php
@@ -4,6 +4,19 @@ return [
     'facts' => [
         'Kittens are adorable.',
         'Kittens are frequently called kittehs.',
-        'All kittens are cats, but not all cats are kittens.'
+        'All kittens are cats, but not all cats are kittens.',
+        'The scientific name for a domestic cat is Felis catus.',
+        'Kittens have poorer color vision than humans.',
+        'Playing is important for kittens, as it helps them develop hunting skills.',
+        'It is virtually impossible to blind a cat with a laser pointer.',
+        'Cats become bored with toys they have prevously played with.',
+        'Cats can be found all over the world, regardless of climate.',
+        'Kittens are very social animals.',
+        'Kittens become cats after seven months to a year.',
+        'Kittens and cats will suckle until removed from their mothers.',
+        'Kittens enjoy human companionship.',
+        'Kittens enjoy and seek out dark places to hide.',
+        'It is illegal in many places to give away kittens younger than eight weeks old.',
+        ''
     ]
 ];
